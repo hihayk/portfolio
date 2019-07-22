c97fd05d06e251fadfdf76c969b79eecb5280dd8
hihayk <say@hihayk.com>
2019-07-22 08:49:56 +0200

    simplify spacer

--- src/components/spacer.js
+++ src/components/spacer.js
@@ -6,48 +6,28 @@ const getSpaceCss = (props) => {
 
 	if(props.top) {
 		paddingSide = 'top'
-
-		if(isNaN(props.top)) {
-			paddingValue = props.top
-		} else (
-			paddingValue = `${props.top * 8}px`
-		)
+		paddingValue = isNaN(props.top) ? props.top : `${props.top * 8}px`
 
 		return `padding-${paddingSide}: ${paddingValue}`
 	}
 	
 	if(props.right) {
 		paddingSide = 'right'
-
-		if(isNaN(props.right)) {
-			paddingValue = props.right
-		} else (
-			paddingValue = `${props.right * 8}px`
-		)
+		paddingValue = isNaN(props.right) ? props.right : `${props.right * 8}px`
 
 		return `padding-${paddingSide}: ${paddingValue}`
 	}
 	
 	if(props.bottom) {
 		paddingSide = 'bottom'
-
-		if(isNaN(props.bottom)) {
-			paddingValue = props.bottom
-		} else (
-			paddingValue = `${props.bottom * 8}px`
-		)
+		paddingValue = isNaN(props.bottom) ? props.bottom : `${props.bottom * 8}px`
 
 		return `padding-${paddingSide}: ${paddingValue}`
 	}
 	
 	if(props.left) {
 		paddingSide = 'left'
-
-		if(isNaN(props.left)) {
-			paddingValue = props.left
-		} else (
-			paddingValue = `${props.left * 8}px`
-		)
+		paddingValue = isNaN(props.left) ? props.left : `${props.left * 8}px`
 
 		return `padding-${paddingSide}: ${paddingValue}`
 	}
